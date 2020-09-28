af997bd32ead87d908d3c6f6c534bb99f3d2755a
NazgulFS <59717278+NazgulFS@users.noreply.github.com>
2020-09-28 14:31:17 -0300

    Updates

---
+++ precache-manifest.a0a4f536ae23ba7e19be9ffbdc043e2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b89d8bb240a4218cd58d23764a79d680",
+    "url": "/NazgulFS/Buscador-de-bebidas/index.html"
+  },
+  {
+    "revision": "4783df77dff295559ade",
+    "url": "/NazgulFS/Buscador-de-bebidas/static/css/2.dc17f35a.chunk.css"
+  },
+  {
+    "revision": "14025be9967d6da2d085",
+    "url": "/NazgulFS/Buscador-de-bebidas/static/css/main.a2d3df89.chunk.css"
+  },
+  {
+    "revision": "4783df77dff295559ade",
+    "url": "/NazgulFS/Buscador-de-bebidas/static/js/2.96cd017d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/NazgulFS/Buscador-de-bebidas/static/js/2.96cd017d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14025be9967d6da2d085",
+    "url": "/NazgulFS/Buscador-de-bebidas/static/js/main.38f03a04.chunk.js"
+  },
+  {
+    "revision": "e3aae926318ac708780d",
+    "url": "/NazgulFS/Buscador-de-bebidas/static/js/runtime-main.4d512d64.js"
+  }
+]);
